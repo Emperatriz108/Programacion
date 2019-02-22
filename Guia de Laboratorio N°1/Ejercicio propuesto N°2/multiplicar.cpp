4ec87f7dc7dd151add55e67b7efa27c04fe3785e
Emperatriz108 <43799487+Emperatriz108@users.noreply.github.com>
2019-02-22 17:37:06 -0600

    Update and rename main.cpp to multiplicar.cpp

--- Guia de Laboratorio N°1/Ejercicio propuesto N°2/multiplicar.cpp
+++ Guia de Laboratorio N°1/Ejercicio propuesto N°2/multiplicar.cpp
@@ -3,24 +3,25 @@
 //Programacion II
 //Hecho por: Gema Emperatriz Manzano Machuca
 //Fecha de creacion: 17/02/2019
-//Objetivo: Escribir una función que multiplique un valor por 10 y devuelva el valor modificado.
+//Objetivo: Escribir una funciÃ³n que multiplique un valor por 10 y devuelva el valor modificado.
 #include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
-void Multiplicar(int a, int b);
+void Multiplicar(int a, int b);//declaracion de la funcion
 
 int main(int argc, char** argv) {
-	int num;
+	int num;//declaracion de la variable
 	cout<<"Ingrese un numero: "<<endl;
-	cin>>num;
+	cin>>num;//valor de num
 	cout<<endl;
 	
-	Multiplicar(num, 10);
+	Multiplicar(num, 10);//llamada de la funcion
 	
 	return 0;
 }
 
+//acciones de la funcion
 void Multiplicar(int a, int b){
 	a = a * b;
 	cout<<"El valor modificado: "<<a;
